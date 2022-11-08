aaffddb37fdefd12d83a4239e2fa0feac45929fa
Mahsa Oshaghi <mahsa.oshaghi@gmail.com>
2022-11-08 14:09:11 +0330

    initial comit

---
+++ src/test/java/com/example/studentmanagements/StudentmanagementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentmanagements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentmanagementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
